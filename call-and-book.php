0688ab5070f73e886d5159b1ded3eed14d94c1c0
msalerno82 <msalerno82@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-12 13:38:39 +0000

    Css fix
    git-svn-id: https://plugins.svn.wordpress.org/callbook-mobile-bar/tags/1.1.1@1197489 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- call-and-book.php
+++ call-and-book.php
@@ -96,12 +96,12 @@ load_plugin_textdomain( 'call_book', FALSE, basename( dirname( __FILE__ ) ) . '/
 		echo $text_hover?> !important; text-decoration:none; }
 		span.callbook-icona-busta-lettera:hover { color:<?php echo
 		$mail_icon_hover ?> !important; } #callbook{ background:<?php echo
-		$color_bg ?>; } a.actioncall, a.actionbook, a.icon{ color:<?php echo $color_text ?>; }
+		$color_bg ?> !important; } a.actioncall, a.actionbook, a.icon{ color:<?php echo $color_text ?>; }
 		span.callbook-icona-telefono, span.callbook-icona-calendario,span.callbook-icona-offerte,
 		span.callbook-icona-acquista,span.callbook-icona-mappa-localita,span.callbook-icona-gallery,span.callbook-icona-info{
 		font-size:<?php echo $icon_size ?>; }span.callbook-align{ font-size:<?php echo $font_size ?>; }
 		span.callbook-icona-busta-lettera { color:<?php echo $mail_icon_color
-		?>; }.callbook_under{ background:<?php echo $color_mail_bg ?>;
+		?>; }.callbook_under{ background:<?php echo $color_mail_bg ?>
 		!important; } .cb_powered a:hover{color:<?php
 		echo $text_hover?>;} #callbook > a > span.callbook-align, .cb_powered a {font-family:<?php echo $font_name
 		?>}</style>
